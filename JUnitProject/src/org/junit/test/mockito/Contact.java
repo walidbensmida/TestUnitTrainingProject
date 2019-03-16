40416242d1345765e7f18ed368ddfb7613ae5aba
walid <walid@DESKTOP-ELKO83N>
2019-03-16 13:10:47 +0100

    Junit DAO 2

--- JUnitProject/src/org/junit/test/mockito/Contact.java
+++ JUnitProject/src/org/junit/test/mockito/Contact.java
@@ -24,21 +24,4 @@ public class Contact {
    public void setId(long id) { this.id = id; }
 }
 
-class ContactDAO {
-	public Contact find(long id) {
-		throw new UnsupportedOperationException();
-	}
 
-	class ContactService {
-		private ContactDAO contactDAO;
-
-		public ContactService(ContactDAO contactDAO) {
-			this.contactDAO = contactDAO;
-		}
-
-		public Contact find(long id) {
-			return contactDAO.find(id);
-		}
-	}
-
-}
